0e8c357dce8351fbd3b816d5d8c904fd6dff3941
veroxcrown <eldav.corona@gmail.com>
2021-01-11 18:28:01 -0600

    practica 1

---
+++ index.js
@@ -0,0 +1,6 @@
+//const moment = require('moment')
+//console.log('hola koders',moment(new Date(),'yyyy-mm-dd hh:mm').toString())
+
+const colors = require('colors')
+console.log('Hola Veronica Corona'.rainbow); // rainbow
+console.log(colors.rainbow('Hola Veronica Corona')); // rainbow
